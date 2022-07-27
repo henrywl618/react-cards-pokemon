075c6950cb35908a144348393a046258eda34c0a
Henry <henrywlam+github@gmail.com>
2022-07-27 16:49:13 -0400

    added custom hooks

--- src/PlayingCardList.js
+++ src/PlayingCardList.js
@@ -3,22 +3,30 @@ import uuid from "uuid";
 import axios from "axios";
 import PlayingCard from "./PlayingCard";
 import "./PlayingCardList.css";
+import { useAxios } from "./hooks";
 
 /* Renders a list of playing cards.
  * Can also add a new card at random. */
 function CardTable() {
   const [cards, setCards] = useState([]);
-  const addCard = async () => {
-    const response = await axios.get(
-      "https://deckofcardsapi.com/api/deck/new/draw/"
-    );
+  const [response, getData] = useAxios("https://deckofcardsapi.com/api/deck/new/draw/");
+
+  const addCard = () => {
+    getData();
     setCards(cards => [...cards, { ...response.data, id: uuid() }]);
+    
   };
+
+  const removeCards = ()=>{
+    setCards([]);
+  };
+  
   return (
     <div className="PlayingCardList">
       <h3>Pick a card, any card!</h3>
       <div>
         <button onClick={addCard}>Add a playing card!</button>
+        <button onClick={removeCards}>Remove all cards</button>
       </div>
       <div className="PlayingCardList-card-area">
         {cards.map(cardData => (
